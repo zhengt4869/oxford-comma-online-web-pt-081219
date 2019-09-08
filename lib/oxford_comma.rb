26e3d488840c86479582d435e886fa7f307025bd
KG ZZ <czheng5h88@gmail.com>
2019-09-08 22:20:40 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    return array.first 
+  end 
+  if array.length == 2 
+    return array.first + " and " + array[1]
+  end 
+  last_string = array.pop
+  res = array.join(", ")
+  res += ", and " + last_string
+  return res
 end
