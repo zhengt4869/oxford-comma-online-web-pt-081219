6a3c3b4905304e4fb9e2d4a2b813cd6701d89cbc
KG ZZ <czheng5h88@gmail.com>
2019-09-08 22:20:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
+  if array.length == 1 
+    return array.first 
+  end 
+  if array.length == 2 
+    return array.first + "and " + array[1]
+  end 
   last_string = array.pop
   res = array.join(", ")
   res += ", and " + last_string
